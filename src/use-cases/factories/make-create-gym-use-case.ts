bf2cc47dc52a6c3c518606cb710a725fa1cab467
Camila <camilinhaalmeida55@gmail.com>
2025-08-26 17:13:03 -0300

    🔧 Add use case factories

---
+++ src/use-cases/factories/make-create-gym-use-case.ts
@@ -0,0 +1,9 @@
+import { CreateGymCase } from '../create-gym'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+
+export function makeCreateGymUseCase() {
+  const gymsRepository = new PrismaGymsRepository()
+  const useCase = new CreateGymCase(gymsRepository)
+
+  return useCase
+}
---
+++ src/use-cases/factories/make-search-gyms-use-case.ts
@@ -0,0 +1,9 @@
+import { SearchGymsCase } from '../search-gyms'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+
+export function makeSearchGymsUseCase() {
+  const gymsRepository = new PrismaGymsRepository()
+  const useCase = new SearchGymsCase(gymsRepository)
+
+  return useCase
+}
